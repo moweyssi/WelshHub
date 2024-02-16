f00208169db624ebd7ce6924dd7911902f720f20
Maxim <maxim.oweyssi@est.org.uk>
2024-02-16 16:18:03 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,39 @@ import pandas as pd
 from openai import OpenAI
 import openai
 import pandas as pd
-openai.api_key="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"
+import streamlit as st
+
+st.title("Welsh AI assistant")
+
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("How can I help?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        )
+        response = st.write_stream(stream)
+
+    st.session_state.messages.append({"role": "assistant", "content": response})
 
 
 
@@ -16,24 +48,7 @@ openai.api_key="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"
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+"""
 
 
 client = OpenAI()
@@ -103,3 +118,5 @@ while True:
         print(pageno)
         print(response_text)
         break
+        
+        """
